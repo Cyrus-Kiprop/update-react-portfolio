ed29d79a4a6fc29eb186bab8dde74335872fbadc
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-11 14:17:23 +0300

    add the projects components

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 import Navbar from "../navbar/navbar";
-import Header from '../header/Header';
+import Header from "../header/Header";
+import Portfolio from "../Portfolio/Portfolio";
+
+import "./Homepage.css";
 
 export default function Homepage() {
-    return (
-        <div>
-            <Navbar/>
-            <Header/>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <main>
+        <Portfolio />
+      </main>
+    </div>
+  );
 }
---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Portfolio.css";
+export default function Portfolio() {
+  return (
+    <section className="portfolio">
+      <h1 className="portfolio_title">My Portfolio</h1>
+      <div className="portfolio__items">
+        <div className="portfolio__item">
+          <div className="portfolio__item-img">
+            <img src="" alt="" />
+          </div>
+          <div className="stack-container">
+            <div className="portfolio__item-stack"> HTML/CSS</div>
+            <div className="portfolio__item-stack"> Ruby on Rails</div>
+            <div className="portfolio__item-stack">Javascript</div>
+          </div>
+          <div className="portfolio__view-item">
+            <Link to="#">See the project</Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
