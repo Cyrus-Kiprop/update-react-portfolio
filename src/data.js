f4ebeff269537015ac6d7d7871819e9a820e74bf
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-16 23:46:17 +0300

    add health app to list of projects

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
-import image from "./assets/images/projects/star-wars.png";
+import image from "./assets/images/projects/api-desktop.png";
 
-import img1 from "./assets/images/projects/game-play.png";
-import img2 from "./assets/images/projects/game-menu.png";
+import img1 from "./assets/images/projects/api-doc-login.png";
+import img2 from "./assets/images/projects/api-mobile-doc.png";
 const projects = [
   {
     name: "Well-paid-geek-bot",
@@ -20,13 +20,38 @@ const projects = [
   },
   {
     name: "Gaurdian of the Galaxies",
-    url: image,
-    exUrl: [image, img1, img2],
+    url:
+      "https://github.com/Cyrus-Kiprop/update-react-portfolio/raw/master/src/assets/images/projects/star-wars.png",
+    exUrl: [
+      "https://github.com/Cyrus-Kiprop/update-react-portfolio/raw/master/src/assets/images/projects/star-wars.png",
+
+      "https://github.com/Cyrus-Kiprop/update-react-portfolio/raw/master/src/assets/images/projects/game-play.png",
+
+      "https://github.com/Cyrus-Kiprop/update-react-portfolio/raw/master/src/assets/images/projects/game-menu.png",
+    ],
     description:
       "Guardian of the Galaxy is a shooter game inspired by the guardian of the Galaxy movie. The game is browser based, and designed on the Phaser 3 game library. The game uses a collection of amazing art designed by various game designers from OpenGameArt website( Please Refer to the acknowlegment section for more Informations). The main goal of this game is to score the highest possible score, while killing invading alien ships.",
     stack: ["Javascript", "Phaser-3", "Jest", "ES6"],
     live: "https://gallant-wescoff-7173eb.netlify.app/",
     github: "https://github.com/Cyrus-Kiprop/phaser-shooting-game",
+  },
+
+  {
+    name: "Health App",
+    url: image,
+    exUrl: [img1, image, img2],
+    description:
+      "A progressive Health Tracking mobile web app. User can utilize the apps' exposed api utility to view and keep track of their fitness levels.",
+    stack: [
+      "Ruby on Rails",
+      "Rspec",
+      "Json-Serializers",
+      "Shoulda-matchers",
+      "Api-documentation",
+    ],
+    live:
+      "https://web.postman.co/collections/8007314-882ee6da-afb0-49db-9eaa-4e9c94059972?version=latest&workspace=ab4298f5-d0f8-4bf1-8dc4-baa6db7fbe13",
+    github: "https://github.com/Cyrus-Kiprop/rails-react-health-app",
   },
 ];
 
