4ddbfd5a3e896c9062b30d2da299dd3c92085e77
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-09-17 00:33:26 +0300

    update the list of projects

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
-import image from "./assets/images/projects/api-desktop.png";
+import image from "./assets/images/projects/reverse-ror.png";
 
-import img1 from "./assets/images/projects/api-doc-login.png";
-import img2 from "./assets/images/projects/api-mobile-doc.png";
+import img1 from "./assets/images/projects/ror-scaffold.png";
+import img2 from "./assets/images/projects/ror-social.png";
 const projects = [
   {
     name: "Well-paid-geek-bot",
@@ -38,8 +38,13 @@ const projects = [
 
   {
     name: "Health App",
-    url: image,
-    exUrl: [img1, image, img2],
+    url:
+      "https://github.com/Cyrus-Kiprop/update-react-portfolio/raw/master/src/assets/images/projects/api-desktop.png",
+    exUrl: [
+      "https://github.com/Cyrus-Kiprop/update-react-portfolio/raw/master/src/assets/images/projects/api-doc-login.png",
+      "https://github.com/Cyrus-Kiprop/update-react-portfolio/raw/master/src/assets/images/projects/api-desktop.png",
+      "https://github.com/Cyrus-Kiprop/update-react-portfolio/raw/master/src/assets/images/projects/api-mobile-doc.png",
+    ],
     description:
       "A progressive Health Tracking mobile web app. User can utilize the apps' exposed api utility to view and keep track of their fitness levels.",
     stack: [
@@ -52,6 +57,43 @@ const projects = [
     live:
       "https://web.postman.co/collections/8007314-882ee6da-afb0-49db-9eaa-4e9c94059972?version=latest&workspace=ab4298f5-d0f8-4bf1-8dc4-baa6db7fbe13",
     github: "https://github.com/Cyrus-Kiprop/rails-react-health-app",
+  },
+  {
+    name: "Private Events",
+    url: image,
+    exUrl: [img1, image, img2],
+    description:
+      "The project involves creation of a private events website(EventBrite like in functionality) in rails. The app implements authentication, rails scopes and association. Many to many relationship is the main key feature. **The project contain database models and schemas that model the user and the post object. visit The Odin Project 'Members Only' for the full project specifications.",
+    stack: ["Ruby on Rails", "Rspec", "Devise", "HTML", "CSS"],
+    live:
+      "https://web.postman.co/collections/8007314-882ee6da-afb0-49db-9eaa-4e9c94059972?version=latest&workspace=ab4298f5-d0f8-4bf1-8dc4-baa6db7fbe13",
+    github: "https://github.com/Cyrus-Kiprop/private-events",
+  },
+
+  {
+    name: "Facebook clone Tutorial",
+    url: image,
+    exUrl: [img1, image, img2],
+    description:
+      "A facebook clone app built using Ruby on rails. The app tries to replicate facebook complex data models required as well as well as delivering a  properly secured app. The  app is a tutorial series code base where starters can clone and get started easily without worrying on association concepts",
+    stack: [
+      "Ruby on Rails",
+      "Rspec",
+      "Minitest",
+      "Postgresql",
+      "Authentication",
+    ],
+    live: "https://agile-mountain-91033.herokuapp.com/users/sign_in",
+    github: "https://github.com/Cyrus-Kiprop/ror-social-scaffold",
+  },
+  {
+    name: "Bookstore (In progress)",
+    url: image,
+    exUrl: [img1, image, img2],
+    description: "A Boostore app created using react/redux. ",
+    stack: ["React", "Redux", "Hooks", "ES6"],
+    live: "https://bookstore-react-redux-ck-fh.herokuapp.com/",
+    github: "https://github.com/cyrus-kiprop/bookstore-react-redux",
   },
 ];
 
